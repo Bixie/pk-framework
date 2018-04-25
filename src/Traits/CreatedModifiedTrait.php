0bbd817378dc8494b288ff2382d0a0831d0ee61f
Matthijs Alles <admin@bixie.nl>
2018-04-25 10:33:18 +0200

    customer statuses

--- src/Traits/CreatedModifiedTrait.php
+++ src/Traits/CreatedModifiedTrait.php
@@ -5,16 +5,28 @@ namespace Bixie\PkFramework\Traits;
 
 trait CreatedModifiedTrait {
 
-	/** @Column(type="integer") */
+    /**
+     * @Column(type="integer")
+     * @var int
+     */
 	public $created_by = 0;
 
-	/** @Column(type="datetime") */
+    /**
+     * @Column(type="datetime")
+     * @var \DateTime
+     */
 	public $created = '';
 
-	/** @Column(type="integer") */
+    /**
+     * @Column(type="integer")
+     * @var int
+     */
 	public $modified_by = 0;
 
-	/** @Column(type="datetime") */
+    /**
+     * @Column(type="datetime")
+     * @var \DateTime
+     */
 	public $modified = '';
 
 }
